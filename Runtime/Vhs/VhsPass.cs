f8a2d09e9317a2245fa5ba95086303690e4b8f03
NullTale <nulltale@gmail.com>
2024-06-27 12:33:57 +0300

    Tape mode regression

--- Runtime/Vhs/VhsPass.cs
+++ Runtime/Vhs/VhsPass.cs
@@ -20,21 +20,12 @@ namespace VolFx
 		private static readonly int s_Noise      = Shader.PropertyToID("_Noise");
 		private static readonly int s_Flickering = Shader.PropertyToID("_Flickering");
 
-        [Tooltip("Use single tape type to smaller build size")]
-        public Optional<Mode> _singleTape;
-		[Tooltip("Default Use tape texture as a negative")]
-		public bool  _negative;
 		[Tooltip("Default Glitch color")]
 		public Color _glitch = Color.red;
 		[HideInInspector]
 		public  float       _frameRate = 20f;
 		[HideInInspector]
-        public  Texture2D[] _tape;
-		[HideInInspector]
-        public  Texture2D[] _noise;
-		[HideInInspector]
-        public  Texture2D[] _shades;
-        private  Texture2D[] _clip;
+        public  Texture2D[] _clip;
 		
 		private float _playTime;
 		private float _yScanline;
@@ -59,37 +50,12 @@ namespace VolFx
 			if (isActive == false)
                 return false;
 			
-			var mode = settings._mode.value;
-			_clip = mode switch
-			{
-				Mode.Tape   => _tape,
-				Mode.Noise  => _noise,
-				Mode.Shades => _shades,
-				_           => throw new ArgumentOutOfRangeException()
-			};
-			if (_singleTape.Enabled)
-			{
-#if UNITY_EDITOR
-				if (settings._mode.overrideState && settings._mode.value != _singleTape.Value)
-					Debug.LogWarning($"VhsFx : Single tape mode is enabled, but effect uses another mode value from volume (mode override)");
-#endif
-				_clip = _singleTape.Value switch
-				{
-					Mode.Tape   => _tape,
-					Mode.Noise  => _noise,
-					Mode.Shades => _shades,
-					_           => throw new ArgumentOutOfRangeException()
-				};
-			}
-
             // scale line
 			_yScanline += Time.deltaTime * 0.01f * settings._bleed.value;
 			_xScanline -= Time.deltaTime * 0.1f * settings._bleed.value;
             
 			var glitch = settings._glitch.overrideState ? settings._glitch.value : _glitch;
-			
-			var neg = settings._negative.overrideState ? settings._negative.value : _negative;
-			
+						
 			if (_yScanline >= 1)
 				_yScanline = Random.value;
             
@@ -102,7 +68,7 @@ namespace VolFx
 			mat.SetFloat(s_Rocking, settings._rocking.value * settings._weight.value);
 			mat.SetColor(s_Glitch, glitch);
 			mat.SetFloat(s_Flickering, settings._flickering.value);
-			mat.SetFloat(s_Tape, neg ? -settings._tape.value : settings._tape.value);
+			mat.SetFloat(s_Tape, settings._tape.value);
 			mat.SetFloat(s_Noise, Mathf.Lerp(1000, 2, settings._noise.value));
             
             // params
@@ -112,51 +78,17 @@ namespace VolFx
             return true;
         }
 
-        protected override bool _editorValidate => _clip == null || _clip.Length == 0 || (Application.isPlaying == false && _clip.Any(n => n == null))
-												   || ((_singleTape.Enabled && _tape != null && _noise != null && _shades != null) || (_singleTape.Enabled == false && (_tape == null || _noise == null || _shades == null)));
+        protected override bool _editorValidate => _clip == null || _clip.Length == 0 || (Application.isPlaying == false && _clip.Any(n => n == null));
         protected override void _editorSetup(string folder, string asset)
         {
 #if UNITY_EDITOR
 			var sep = Path.DirectorySeparatorChar;
 			
-			_tape = UnityEditor.AssetDatabase.FindAssets("t:texture", new string[] {$"{folder}{sep}Vhs{sep}Tape"})
-							   .Select(n => UnityEditor.AssetDatabase.LoadAssetAtPath<Texture2D>(UnityEditor.AssetDatabase.GUIDToAssetPath(n)))
-							   .Where(n => n != null)
-							   .ToArray();
-			_noise = UnityEditor.AssetDatabase.FindAssets("t:texture", new string[] {$"{folder}{sep}Vhs{sep}Noise"})
-							   .Select(n => UnityEditor.AssetDatabase.LoadAssetAtPath<Texture2D>(UnityEditor.AssetDatabase.GUIDToAssetPath(n)))
-							   .Where(n => n != null)
-							   .ToArray();
-			_shades = UnityEditor.AssetDatabase.FindAssets("t:texture", new string[] {$"{folder}{sep}Vhs{sep}Shades"})
+			_clip = UnityEditor.AssetDatabase.FindAssets("t:texture", new string[] {$"{folder}{sep}Vhs{sep}Tape"})
 							   .Select(n => UnityEditor.AssetDatabase.LoadAssetAtPath<Texture2D>(UnityEditor.AssetDatabase.GUIDToAssetPath(n)))
 							   .Where(n => n != null)
 							   .ToArray();
 							   
-			if (_singleTape.Enabled)
-			{
-				switch (_singleTape.Value)
-				{
-					case Mode.Tape:
-					{
-						_noise  = null;
-						_shades = null;
-					} break;
-					case Mode.Noise:
-					{
-						_tape   = null;
-						_shades = null;
-					} break;
-					case Mode.Shades:
-					{
-						_tape   = null;
-						_noise  = null;
-					} break;
-					default:
-						throw new ArgumentOutOfRangeException();
-				}
-			}
-			
-			_clip     = _tape ?? _noise ?? _shades;
 			_playTime = 0f;
 #endif
         }
--- Runtime/Vhs/VhsVol.cs
+++ Runtime/Vhs/VhsVol.cs
@@ -13,11 +13,9 @@ namespace VolFx
         public ClampedFloatParameter _bleed      = new ClampedFloatParameter(1f, 0f, 10f);
         public ClampedFloatParameter _rocking    = new ClampedFloatParameter(0f, 0f, 0.1f);
         public ClampedFloatParameter _tape       = new ClampedFloatParameter(1f, 0f, 2f);
-        public BoolParameter         _negative   = new BoolParameter(false);
         public ClampedFloatParameter _noise      = new ClampedFloatParameter(.5f, 0f, 1f);
         public ClampedFloatParameter _flickering = new ClampedFloatParameter(0f, 0f, 2f);
         public ColorParameter        _glitch     = new ColorParameter(new Color(1f, 0f, 0f, 1));
-        public ModeParameter         _mode       = new ModeParameter(VhsPass.Mode.Tape, false);
         
         // =======================================================================
         [Serializable]
